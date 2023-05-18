942ca5790ee40b3cd325b6a4d3ca1de0ccd07447
Jonathan Campos <Malvyx5@gmail.com>
2023-05-18 13:53:00 -0400

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+
+function saturdayFun(roller = "roller-skate") {
+    return `This Saturday, I want to ${roller}!`;
+}
+
+function mondayWork(office = "go to the office") {
+    return `This Monday, I will ${office}.`;
+}
+
+function wrapAdjective(result = "*") {
+    const innerFunction = function (emphatic = "special") {
+        return `You are ${result}${emphatic}${result}!`;
+    };
+    return innerFunction;
+}
